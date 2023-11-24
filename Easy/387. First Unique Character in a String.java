94a6000500e86a0afb8a9d85480a5a5b2515a62f
parthsingh <parth119516@gmail.com>
2023-11-23 17:24:44 -0800

    Create 387. First Unique Character in a String.java

---
+++ Easy/387. First Unique Character in a String.java
@@ -0,0 +1,56 @@
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters
+*/
+
+class Solution {
+    public int firstUniqChar(String s) 
+    {
+        HashMap<Character, Integer> values = new HashMap<>();
+
+        for(int i = 0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+
+            if(values.containsKey(c))
+            {
+                values.put(c,values.get(c) + 1);
+            }
+            else
+            {
+                values.put(c,1);
+            }
+        }        
+
+        for(int i = 0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+
+            if(values.get(c) == 1)
+                return i;
+        }
+
+        return -1;
+
+    }
+}
